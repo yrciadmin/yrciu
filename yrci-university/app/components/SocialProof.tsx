8a077ca3f5e4a2a4646b70c1d39054640ab8fdde
yrciadmin <lpresident@yrci.com>
2024-03-26 23:18:35 -0500

    completed social proof section

---
+++ yrci-university/app/components/SocialProof.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SocialProof = () => {
+  return (
+    <div className='flex justify-center items-center h-28 w-full bg-dark-teal mt-8'>
+        <div className='flex justify-around w-full text-white font-semibold text-xl'>
+            <p>AmeriCorps</p>
+            <p>USDA ARS</p>
+            <p>U.S. Department of Commerce</p>
+            <p>NOAA</p>
+            <p>U.S. Patent and Trademark </p>
+        </div>
+    </div>
+  )
+}   
+
+export default SocialProof
--- yrci-university/app/page.tsx
+++ yrci-university/app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "./components/Hero";
+import SocialProof from "./components/SocialProof";
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
      <Hero/>
-    // </main>
+     <SocialProof/>
+    </>
+    
   );
 }
