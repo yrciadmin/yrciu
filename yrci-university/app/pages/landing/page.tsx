45b1e3b8c1b5c191d607d73d1dc6e3e672b2ace8
yrciadmin <lpresident@yrci.com>
2024-06-18 08:34:37 -0500

    fixed reviews container added additional courses

--- yrci-university/app/pages/landing/page.tsx
+++ yrci-university/app/pages/landing/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ContactUs from "../../components/ContactUs";
 import FeaturedCourses from "../../components/FeaturedCourses";
 import Hero from "../../components/Hero";
@@ -6,18 +7,18 @@ import WhyChooseUs from "../../components/WhyChooseUs";
 import Instructors from "../../components/Instructors";
 import Reviews from "../../components/Reviews";
 import GroupPrice from "../../components/GroupPrice";
+
 export default function Home() {
   return (
     <>
-     <Hero/>
-     <SocialProof/>
-     <WhyChooseUs/>
-     <FeaturedCourses/>
-     {/* <Instructors/> */}
-     <Reviews/>
-     <GroupPrice/>
-     {/* <ContactUs/> */}
+      <Hero />
+      <SocialProof />
+      <WhyChooseUs />
+      <FeaturedCourses />
+      {/* <Instructors /> */}
+      <Reviews />
+      <GroupPrice />
+      {/* <ContactUs /> */}
     </>
-    
   );
 }
