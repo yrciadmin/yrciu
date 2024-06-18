a84de805200902e6e25e6d13485950b8a29ea57a
yrciadmin <lpresident@yrci.com>
2024-06-18 14:24:22 -0500

    finished contact form and terms of agreements

--- yrci-university/app/components/ContactUs.tsx
+++ yrci-university/app/components/ContactUs.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
 import Heading from '../ui/Typography/Heading'
 import Input from '../ui/Forms/Input'
 import Paragraph from '../ui/Typography/Paragraph'
+import Modal from '../ui/Modal'
+import { termsAndConditions } from '../util/data'
 
 //borders border-4 xs:border-orange-500  sm:border-slate-500   md:border-mustard-yellow  lg:border-indigo-500 xl:border-green-500  2xl:border-yrci-red
 const ContactUs = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <section className='flex items-center bg-muted-turquise md:bg-contact-us bg-no-repeat bg-cover rounded-3xl sm:bg-center justify-center  max-w-[1462px] '>
       <div className=' flex flex-col w-full '>
@@ -15,10 +20,24 @@ const ContactUs = () => {
               <div className='mt-8 xs:mt-2 sm:mt-0 flex justify-center items-center w-full'>
                 <Input/>
               </div>
-              <div className='flex pb-4 justify-center'><Paragraph className='text-dark-teal text-sm mt-4 w-11/12 md:w-8/12 text-center'>We're committed to your privacy. YRCI uses the information you provide to us to contact you about our content, products, and services. You may unsubscribe from these communications at any time.</Paragraph></div>
+              <div className='flex pb-4 justify-center'>
+                <Paragraph className='text-dark-teal text-sm mt-4 w-11/12 md:w-8/12 text-center'>
+                  We're committed to your privacy. YRCI uses the information you provide to us to contact 
+                  you about our content, products, and services. You may unsubscribe from these communications at any time. 
+                  Please read our 
+                  <span
+                    onClick={() => setShowModal(true)}
+                    className=" mx-2 text-[12px] xl:text-[22px] cursor-pointer text-light-gray font-semibold"
+                  >
+                    Terms and Conditions
+                  </span>
+                  for more information.
+                </Paragraph>
+              </div>
               
           </div>
       </div>
+      <Modal show={showModal} onClose={() => setShowModal(false)} content={termsAndConditions} />
     </section>
   )
 }
---
+++ yrci-university/app/ui/Modal.tsx
@@ -0,0 +1,80 @@
+// components/Modal.tsx
+'use client';
+
+import { FC, useRef, useEffect } from 'react';
+import { TermConditionProps } from '../util/data';
+
+interface ModalProps {
+  show: boolean;
+  onClose: () => void;
+  content: TermConditionProps[];
+}
+
+const Modal: FC<ModalProps> = ({ show, onClose, content }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (show) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [show]);
+
+  if (!show) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
+      <div ref={modalRef} className="bg-turquise-shadow rounded-lg shadow-xl w-11/12 md:w-9/12 max-h-full py-8 overflow-hidden">
+        <div className="p-4 overflow-y-auto max-h-[80vh] w-11/12 mx-auto bg-white px-8">
+                <h3 className="text-sm 3xl:text-md mb-2">Terms and Conditions - YRCI University</h3>
+                <h2 className="text-lg font-semibold mb-4 text-center 3xl:text-2xl">
+                    Welcome to YRCI University's E-commerce Website
+                </h2>
+                <p className="text-sm text-gray-600 mb-4">
+                    By accessing or using our website where you can shop for online learning courses and materials offered by YRCI University
+                    (referred to as "YRCI"), you agree to be bound by these Terms and Conditions. Please read them carefully.
+                </p>
+                <div>
+                    {content.map((item, index) => (
+                    <div key={index} className="mb-4">
+                        <h1 className="text-md 3xl:text-xl font-bold mb-4">{item.term}</h1>
+                        <p className="text-sm 3xl:text-lg text-gray-600 mb-2">{item.detail}</p>
+                        {item.bullets && (
+                            <ul className='ml-8'>
+                                {item.bullets.map((bullet) => (
+                                    <li className='list-disc text-sm 3xl:text-base text-gray-600'>{bullet}</li>
+                                ))}
+                            </ul>
+                        )}
+                        
+                    </div>
+                    ))}
+                </div>
+                
+          </div>
+          <div className='flex justify-end w-11/12'>
+          <button
+            onClick={onClose}
+            className="mt-4 px-4 py-2  bg-dark-teal text-white rounded w-28"
+           >
+            Close
+        </button>
+          </div>
+         
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
